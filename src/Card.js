5010ec4bd3b3fe97f8a946d2941e09aaab01e968
lilian-wong <lilianwongai@gmail.com>
2020-02-03 21:16:09 -0500

    add comments

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './Card.css';
+// The Card component should render markup matching the design.html: a div with a class of 'Card' containing an h3 for the card's title and a p element for the card's content.
+// The Card component accepts 2 props: title and content.
+// title is a string of the card's title.
+// content is a string of the card's content.
+// These props will be passed in for each Card from the List component.
 
 class Card extends React.Component{
     constructor(props) {
